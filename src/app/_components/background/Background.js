83cdfa715434d30a47ee6e099c086be1c6c62abf
lmace-eriks <lucasmace4130@gmail.com>
2023-07-10 23:02:49 -0400

    Adding freeze frame fix

--- src/app/_components/background/Background.js
+++ src/app/_components/background/Background.js
@@ -9,23 +9,9 @@ import meInTree from "./meintree.jpg";
 
 export default function Background() {
   const [streamBackground, setStreamBackground] = useState(false);
+  const timer = useRef();
   const videoElement = useRef();
   const stream = useRef(null);
-
-  useEffect(() => {
-    navigator.mediaDevices.addEventListener("devicechange", handleStopStream);
-
-    return () => {
-      navigator.mediaDevices.removeEventListener(
-        "devicechange",
-        handleStopStream
-      );
-    };
-  });
-
-  const handleStopStream = (e) => {
-    console.info(e);
-  };
 
   const getUserMedia = async (constraints) => {
     const deviceIsMobile = window.innerWidth < 1026;
@@ -52,6 +38,24 @@ export default function Background() {
       console.error(e);
     }
   };
+
+  // After much research, I have not been able to find
+  // a "user has turned their camera off" event to
+  // listen for. This is a workaround that I am not
+  // proud of. This prevents the video background from
+  // freezing on the final captured frame if the user
+  // turns off their camera. - LM
+  useEffect(() => {
+    if (!streamBackground) return;
+
+    timer.current = setInterval(() => {
+      if (!stream.current.active) stopUserMedia();
+    }, 2000);
+
+    return () => {
+      clearInterval(timer.current);
+    };
+  });
 
   const stopUserMedia = async () => {
     const videoTrack = await stream.current.getTracks()[0];
