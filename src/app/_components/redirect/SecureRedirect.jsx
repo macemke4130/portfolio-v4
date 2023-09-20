223f0b3e7d175104c8d1d761d68cad7f233434b6
lmace-eriks <lucasmace4130@gmail.com>
2023-09-20 11:53:41 -0400

    Secure Redirect

---
+++ src/app/_components/redirect/SecureRedirect.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function SecureRedirect() {
+  useEffect(() => {
+    const userProtocol = window.location.protocol;
+    
+    if (userProtocol !== "https:") {
+      const userPath = window.location.pathname;
+      
+      if (window.location.hostname === "localhost") return;
+      window.location.replace(`https://www.lucasmace.com${userPath}`);
+    }
+
+  }, []);
+
+  return <></>;
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Navigation from "./_components/navigation/Navigation";
 import Background from "./_components/background/Background";
 import Console from "./_components/console-log/Console";
+import SecureRedirect from "./_components/redirect/SecureRedirect";
 import "./globals.css";
 
 export const metadata = {
@@ -11,20 +12,23 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>
-        <Background />
-        <header>
-          <h1>Lucas Mace</h1>
-          <h2>Full Stack Web Developer</h2>
-        </header>
-        <main className="mainGrid">
-          <div className="gridSpacer"></div>
-          {children}
-        </main>
-        <Navigation />
-        <Console />
-      </body>
-    </html>
+    <>
+      <SecureRedirect />
+      <html lang="en">
+        <body>
+          <Background />
+          <header>
+            <h1>Lucas Mace</h1>
+            <h2>Full Stack Web Developer</h2>
+          </header>
+          <main className="mainGrid">
+            <div className="gridSpacer"></div>
+            {children}
+          </main>
+          <Navigation />
+          <Console />
+        </body>
+      </html>
+    </>
   );
 }
